3986d3fd163e0e6f948ffb58687431fa48ef78a0
Quindarts <quang82thcspb@gmail.com>
2023-12-15 16:58:55 +0700

    fix modal

--- src/Components/ui/Modal/style.js
+++ src/Components/ui/Modal/style.js
@@ -1,68 +1,68 @@
 /** @jsxImportSource @emotion/react */
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
-import { Modal } from '@mui/material';
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import { Modal } from '@mui/material'
 
 const sizes = {
-  'square': { width: 400, height: 400 },
-  'tall': { width: 300, height: 600 },
-  'fat': { width: 500, height: 300 },
-};
+    square: { width: 400, height: 400 },
+    tall: { width: 300, height: 600 },
+    fat: { width: 500, height: 300 },
+}
 
 const animations = {
-  'bottom_left': keyframes`
+    bottom_left: keyframes`
       0% { top: 100%; left: 0; transform: translate(0, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'top': keyframes`
+    top: keyframes`
       0% { top: 0; left: 50%; transform: translate(-50%, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'top_right': keyframes`
+    top_right: keyframes`
       0% { top: 0; left: 100%; transform: translate(0, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'left': keyframes`
+    left: keyframes`
       0% { top: 50%; left: 0; transform: translate(0, -50%); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'right': keyframes`
+    right: keyframes`
       0% { top: 50%; left: 100%; transform: translate(0, -50%); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'bottom_right': keyframes`
+    bottom_right: keyframes`
       0% { top: 100%; left: 100%; transform: translate(0, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'bottom': keyframes`
+    bottom: keyframes`
       0% { top: 100%; left: 50%; transform: translate(-50%, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'top_left': keyframes`
+    top_left: keyframes`
       0% { top: 0; left: 0; transform: translate(0, 0); }
       100% { top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-  'center': keyframes`
+    center: keyframes`
       0% { opacity: 0; top: 50%; left: 50%; transform: translate(-50%, -50%);}
       100% { opacity: 1; top: 50%; left: 50%; transform: translate(-50%, -50%); }
     `,
-};
+}
 
 const MainModal = styled.div(({ size, appearance }) => ({
-  position: 'absolute',
-  ...sizes[size],
-  backgroundColor: '#ffffff',
-  boxShadow: 24,
-  padding: '1rem',
-  animation: `${animations[appearance]} 1s`,
-  animationFillMode: 'forwards',
-  borderRadius: '10px',
-}));
+    position: 'absolute',
+    ...sizes[size],
+    backgroundColor: '#ffffff',
+    boxShadow: 24,
+    padding: '1rem',
+    animation: `${animations[appearance]} 1s`,
+    animationFillMode: 'forwards',
+    borderRadius: '10px',
+}))
 
 const CustomModal = styled(Modal)`
- &{
-  color: white,
- }
+    & {
+        color: white;
+    }
 `
 
 export { MainModal, CustomModal }
