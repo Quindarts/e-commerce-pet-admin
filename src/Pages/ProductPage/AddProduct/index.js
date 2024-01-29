1525038ff6bbf8637f5769eb13d8762b1a644a01
boyandem99 <tt25318803@gmail.com>
2024-01-30 06:16:30 +0700

    update

--- src/Pages/ProductPage/AddProduct/index.js
+++ src/Pages/ProductPage/AddProduct/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
 import { Box, Typography } from '@mui/material'
 import Button from '../../../Components/ui/Button/Button.js'
 import { Card } from '../styles.js'
@@ -16,128 +19,165 @@ const AddProduct = () => {
         { title: 'Brand 1', value: '1' },
         { title: 'Brand 2', value: '2' },
     ])
-    const size = [
-        { title: 'sm', value: '1' },
-        { title: 'lg', value: '2' },
-        { title: 'lg', value: '3' },
-    ]
+
     const [category, setCategory] = useState([
         { title: 'Category 1', value: '1' },
         { title: 'Category 2', value: '2' },
     ])
-
-    const [openBrand, setOpenBrand] = useState(false)
-    const [openCategory, setOpenCategory] = useState(false)
-
-    const handleOpenBrand = () => {
-        setOpenBrand(true)
+    const [attributeList, setAttributeList] = useState([
+        { title: 'Select Attribute', value: '' },
+        { title: 'Create new Attribute', value: 'create_new_attribute' },
+    ])
+    const [providerList, setProviderList] = useState([
+        { title: 'Select Provider', value: '' },
+        { title: 'Create new Provider', value: 'create_new_provider' },
+    ])
+    const [providers, setProviders] = useState([])
+    const addProvider = () => {
+        setProviders([...providers, { name: '', value: '' }])
+    }
+    const [newProviderName, setNewProviderName] = useState('')
+    const handleProviderChange = (event, index) => {
+        const selectedValue = event.target.value
+        if (selectedValue === 'create_new_provider') {
+            setProviderModalOpen(true)
+        } else {
+            const selectedProvider = providerList.find((attr) => attr.value === selectedValue)
+            if (selectedProvider) {
+                const newProviders = [...providers]
+                newProviders[index].name = selectedProvider.title
+                newProviders[index].value = selectedProvider.value
+                setProviders(newProviders)
+            }
+        }
+    }
+    const handleProviderValueChange = (event, index) => {
+        const newProviders = [...providers]
+        newProviders[index].value = event.target.value
+        setProviders(newProviders)
     }
 
-    const handleOpenCategory = () => {
-        setOpenCategory(true)
+    const handleNewProviderNameChange = (event) => {
+        setNewProviderName(event.target.value)
     }
 
-    const handleCloseBrand = () => {
-        setOpenBrand(false)
+    const removeProvider = (index) => {
+        setProviders(providers.filter((_, i) => i !== index))
     }
 
-    const handleCloseCategory = () => {
-        setOpenCategory(false)
+    const cancelNewProvider = () => {
+        setProviderModalOpen(false)
+    }
+    const saveNewProvider = () => {
+        setProviderList([...providerList, { title: newProviderName, value: newProviderName }])
+        setProviderModalOpen(false)
     }
 
-    const handleAddBrand = (title, value) => {
-        setBrand([...brand, { title, value }])
-        handleCloseBrand()
+    const [attributes, setAttributes] = useState([])
+    const addAttribute = () => {
+        setAttributes([...attributes, { name: '', value: '' }])
+    }
+    const [attributeModalOpen, setAttributeModalOpen] = useState(false)
+    const [providerModalOpen, setProviderModalOpen] = useState(false)
+
+    const [newAttributeName, setNewAttributeName] = useState('')
+    const handleAttributeChange = (event, index) => {
+        const selectedValue = event.target.value
+        if (selectedValue === 'create_new_attribute') {
+            setAttributeModalOpen(true)
+        } else {
+            const selectedAttribute = attributeList.find((attr) => attr.value === selectedValue)
+            if (selectedAttribute) {
+                const newAttributes = [...attributes]
+                newAttributes[index].name = selectedAttribute.title
+                newAttributes[index].value = selectedAttribute.value
+                setAttributes(newAttributes)
+            }
+        }
     }
 
-    const handleAddCategory = (title, value) => {
-        setCategory([...category, { title, value }])
-        handleCloseCategory()
+    const handleAttributeValueChange = (event, index) => {
+        const newAttributes = [...attributes]
+        newAttributes[index].value = event.target.value
+        setAttributes(newAttributes)
     }
+
+    const handleNewAttributeNameChange = (event) => {
+        setNewAttributeName(event.target.value)
+    }
+
+    const removeAttribute = (index) => {
+        setAttributes(attributes.filter((_, i) => i !== index))
+    }
+
+    const cancelNewAttribute = () => {
+        setAttributeModalOpen(false)
+    }
+    const handleModalClose = () => {
+        setProviderModalOpen(false)
+    }
+    const saveNewAttribute = () => {
+        setAttributeList([...attributeList, { title: newAttributeName, value: newAttributeName }])
+        setAttributeModalOpen(false)
+    }
+
     return (
-        <Box className=" grid w-full grid-cols-8 ">
-            <Box sx={Card} className=" col-span-4 grid grid-cols-4">
+        <Box className="grid w-full grid-cols-8 gap-7 ">
+            <Box sx={Card} className="col-span-4 grid grid-cols-4 gap-5">
                 <Textfield placeholder="Name" id="name" type="text" label="Name" className=" col-span-4"></Textfield>
                 <Dropdown className=" col-span-4" list={brand} size="xl" />
-                
-
-                <Modal key={1} open={openBrand} appearance="center" handleClose={handleCloseBrand}>
-                    <form
-                        onSubmit={(event) => {
-                            event.preventDefault()
-                            const title = event.target.elements.title.value
-                            const value = event.target.elements.value.value
-                            handleAddBrand(title, value)
-                        }}
-                    >
-                        <label>
-                            Title:
-                            <input type="text" name="title" required />
-                        </label>
-                        <label>
-                            Value:
-                            <input type="text" name="value" required />
-                        </label>
-                        <Button type="submit">Add</Button>
-                    </form>
-                </Modal>
-
                 <Dropdown className="col-span-4" list={category} size="xl" />
-              
-                <Modal key={2} open={openCategory} appearance="center" handleClose={handleCloseCategory}>
-                    <form
-                        onSubmit={(event) => {
-                            event.preventDefault()
-                            const title = event.target.elements.title.value
-                            const value = event.target.elements.value.value
-                            handleAddCategory(title, value)
-                        }}
-                    >
-                        <label>
-                            Title:
-                            <input type="text" name="title" required />
-                        </label>
-                        <label>
-                            Value:
-                            <input type="text" name="value" required />
-                        </label>
-                        <Button type="submit">Add</Button>
-                    </form>
-                </Modal>
-            </Box>
-            <Box sx={Card} className="col-span-4 grid grid-cols-4">
                 <Textfield
-                    placeholder="Stock"
-                    id="stock"
-                    type="number"
-                    label="Stock"
-                    className=" col-span-2"
-                ></Textfield>
-                 <Textfield
                     placeholder="Bought Price"
                     id="bought"
                     type="number"
                     label="Bought Price"
-                    className=" col-span-2"
+                    className=" col-span-4"
                 ></Textfield>
                 <Textfield
                     placeholder="Sell Price"
                     id="sell"
                     type="number"
                     label="Sell Price"
+                    className=" col-span-4"
+                ></Textfield>
+            </Box>
+            <Box sx={Card} className=" col-span-4 grid grid-cols-4 gap-5">
+                <Textfield
+                    placeholder="Stock"
+                    id="stock"
+                    type="number"
+                    label="Stock"
+                    className=" col-span-4"
+                ></Textfield>
+                <Textfield
+                    placeholder="... cm"
+                    id="Height"
+                    type="number"
+                    label="Height"
+                    className=" col-span-2"
+                ></Textfield>{' '}
+                <Textfield
+                    placeholder="... cm"
+                    id="Length"
+                    type="number"
+                    label="Length"
+                    className=" col-span-2"
+                ></Textfield>{' '}
+                <Textfield
+                    placeholder="... cm"
+                    id="Width"
+                    type="number"
+                    label="Width"
                     className=" col-span-2"
                 ></Textfield>
-               
-                <Dropdown className="col-span-2" list={size} size="xl" />
-                <Box className=" col-span-2">
-                    <Textfield
-                        placeholder="... gram"
-                        id="Weight"
-                        type="number"
-                        label="Weight"
-                        className=" col-span-2"
-                    ></Textfield>
-                </Box>
+                <Textfield
+                    placeholder="... gram"
+                    id="Weight"
+                    type="number"
+                    label="Weight"
+                    className=" col-span-2"
+                ></Textfield>
                 <Textfield
                     placeholder="Description"
                     id="description"
@@ -148,6 +188,76 @@ const AddProduct = () => {
             </Box>
             <Box className="col-span-8">
                 <InputUpload className=" h-1 "></InputUpload>
+            </Box>
+            <Box className="col-span-8">
+                <Accordion>
+                    <AccordionSummary aria-controls="panel1-content" id="panel1-header">
+                        Attribute
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {attributes.map((attribute, index) => (
+                            <div key={index}>
+                                <Dropdown
+                                    list={[...attributeList]}
+                                    onChange={(event) => handleAttributeChange(event, index)}
+                                />
+
+                                <Textfield
+                                    label="Enter value here"
+                                    value={attribute.value}
+                                    onChange={(event) => handleAttributeValueChange(event, index)}
+                                />
+                                <Button onClick={() => removeAttribute(index)}>Delete</Button>
+                            </div>
+                        ))}
+                        <Button onClick={addAttribute}>Add Attribute</Button>
+                        <Modal open={attributeModalOpen} onClose={() => setAttributeModalOpen(false)}>
+                            <h2>Add Attribute</h2>
+                            <Textfield
+                                label="Attribute name"
+                                value={newAttributeName}
+                                onChange={handleNewAttributeNameChange}
+                            />
+                            <Button onClick={saveNewAttribute}>Save</Button>
+                            <Button onClick={cancelNewAttribute}>Cancel</Button>
+                        </Modal>
+                    </AccordionDetails>
+                </Accordion>{' '}
+            </Box>
+            <Box className="col-span-8">
+                <Accordion>
+                    <AccordionSummary aria-controls="panel1-content" id="panel1-header">
+                        Provider
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {providers.map((provider, index) => (
+                            <div key={index}>
+                                <Dropdown
+                                    list={[...providerList]}
+                                    onChange={(event) => handleProviderChange(event, index)}
+                                />
+
+                                <Textfield
+                                    label="Enter value here"
+                                    value={provider.value}
+                                    onChange={(event) => handleProviderValueChange(event, index)}
+                                />
+                                <Button onClick={() => removeProvider(index)}>Delete</Button>
+                            </div>
+                        ))}
+                        <Button onClick={addProvider}>Add Provider</Button>
+                        <Modal open={providerModalOpen} onClose={() => setProviderModalOpen(false)}>
+                            <h2>Add Provider</h2>
+                            <Textfield
+                                label="Provider name"
+                                value={newProviderName}
+                                onChange={handleNewProviderNameChange}
+                            />
+                            <Button onClick={saveNewProvider}>Save</Button>
+                            <Button onClick={cancelNewProvider}>Cancel</Button>
+                        </Modal>
+                    </AccordionDetails>
+                </Accordion>
             </Box>
             <Box>
                 <Button onClick={() => navigate(APP_ROUTER.PRODUCT)} size="sm" color="primary" className=" ">
