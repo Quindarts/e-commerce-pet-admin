1af5aac5b236589899d9851c46ee3b24da5c0dfc
boyandem99 <tt25318803@gmail.com>
2024-03-06 16:26:42 +0700

    Menu fixing

---
+++ src/Components/ui/Menu/Details/BasicInfo.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Box} from '@mui/material'
+
+import { Card } from '../style'
+
+export default function BasicInfo() {
+
+    return (
+        <Box sx={Card}>
+           <h4>Basic Information</h4>
+        </Box>
+    )
+}
