b4dbc387809a07e8effaf9eca6ae81a615802543
boyandem99 <tt25318803@gmail.com>
2024-03-08 13:39:28 +0700

    badge update

--- src/Components/ui/Badge/style.js
+++ src/Components/ui/Badge/style.js
@@ -3,11 +3,23 @@ import { styled } from '@mui/material/styles'
 import { createTheme } from '@mui/material/styles'
 import { theme } from '../../../Theme/theme'
 
-export const StyledBadge = styled(({ shape, border, className, ...other }) => (
+const colorMap = {
+    dark_green: { main: theme.color.jade, text: theme.color.green_200 },
+    green: { main: theme.color.green_200, text: theme.color.green_600 },
+    blue: { main: theme.color.badge_blue, text: theme.color.white },
+    red: { main: theme.color.pink_900, text: theme.color.white },
+    red_text: { main: 'transparent', text: theme.color.pink_900 },
+    gray: { main: theme.color.gray_light, text: theme.color.gray_dark },
+    green_text: { main: 'transparent', text: theme.color.jade },
+    pink: { main: theme.color.light_pink, text: theme.color.pink_900 },
+}
+
+export const StyledBadge = styled(({ shape, border, className, size, ...other }) => (
     <Badge className={className} {...other} />
-))(({ shape, border, theme }) => {
+))(({ shape, border, theme, size }) => {
     const borderStyle = border ? `2px solid #fff` : 'none'
-    const borderRadius = shape === 'square' ? '4px' : ''
+    const borderRadius = shape === 'square' ? '100px' : ''
+    const badgeSize = size === 'lg' ? 15 : size === 'md' ? 12 : 10
 
     return {
         '& .MuiBadge-badge': {
@@ -16,19 +28,12 @@ export const StyledBadge = styled(({ shape, border, className, ...other }) => (
             maxWidth: '150px',
             whiteSpace: 'nowrap',
             color: theme.palette.text.primary,
+            width: badgeSize,
+            height: badgeSize,
+            borderRadius: '50%',
         },
     }
 })
-
-const colorMap = {
-    green: { main: theme.color.jade, text: theme.color.white },
-    blue: { main: theme.color.badge_blue, text: theme.color.white },
-    red: { main: theme.color.pink_900, text: theme.color.white },
-    red_text: { main: theme.color.gray_light, text: theme.color.pink_900 },
-    gray: { main: theme.color.gray_light, text: theme.color.gray_dark },
-    green_text: { main: theme.color.gray_light, text: theme.color.jade },
-    pink: { main: theme.color.light_pink, text: theme.color.pink_900 },
-}
 
 export const createDynamicTheme = (type) => {
     const colors = colorMap[type]
